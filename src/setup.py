6f1f2c7035c29a82a7b4081653bbcb6e6fbee8e0
Jun Ma <junma75@gmail.com>
2018-11-12 19:20:04 +0800

    Re-org the file structure.

---
+++ src/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from os.path import join, dirname
+
+here = dirname(__file__)
+
+setup(name='findata',
+      version='0.0.1',
+      description='Financial data access libraries for crypto trading platforms.',
+      long_description=open(join(here, 'README.md')).read(),
+      author='Jun Ma',
+      author_email='junma018@outlook.com',
+      url='',
+      install_requires=[
+          'pandas==0.23.4',
+      ],
+      packages=['huobi',],
+      )
